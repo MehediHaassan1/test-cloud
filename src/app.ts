ed9f9bbbdfb530807324426eaf460f1a27cb794d
mehedihassanm360ict <mehedi.haassan1@gmail.com>
2026-05-07 19:18:26 +0600

    add error route

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,20 @@ app.use(express.json());
 // static file serve
 app.use(express.static(path.join(__dirname, "../public")));
 
+app.get("/", (req: Request, res: Response) => {
+	res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 // API route
 app.get("/api/hello", (req: Request, res: Response) => {
 	res.json({
 		message: "Hello from Express + TypeScript 🚀",
+	});
+});
+
+app.get("/api/error", (req: Request, res: Response) => {
+	res.status(500).json({
+		error: "This is a simulated error response.",
 	});
 });
 
